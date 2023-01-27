fdf8f2016445329ac7dccf06484360722556ef6f
kirill <kirill.zubov96@yandex.ru>
2023-01-27 21:26:33 +0300

    edit

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        CsvParser parser = context.getBean(CsvParser.class);
-        List<Question> questions = parser.readQuestionsFromFile();
-        questions.stream().forEach(question -> System.out.println(question.toString()));
+        new ClassPathXmlApplicationContext("/spring-context.xml")
+                .getBean(CsvParser.class)
+                .readQuestionsFromFile().stream()
+                .forEach(question -> System.out.println(question.toString()));
     }
 }
